f5fe5f23cd906577a218a0b5c1b21d4625b1650f
Aluno <Aluno>
2017-09-11 15:40:26 -0300

    Projeto inicial do dropbox

--- index.php
+++ index.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-  </body>
+<html>
+<head>
+	<title>PHP Simple Chat System using AJAX</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+	#login_form{
+		width:350px;
+		height:350px;
+		position:relative;
+		top:50px;
+		margin: auto;
+		padding: auto;
+	}
+</style>
+</head>
+<body>
+<div class="container">
+	<div id="login_form" class="well">
+		<h2><center><span class="glyphicon glyphicon-lock"></span> Please Login</center></h2>
+		<hr>
+		<form method="POST" action="login.php">
+		Username: <input type="text" name="username" class="form-control" required>
+		<div style="height: 10px;"></div>		
+		Password: <input type="password" name="password" class="form-control" required> 
+		<div style="height: 10px;"></div>
+		<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Login</button> No account? <a href="signup.php"> Sign up</a>
+		</form>
+		<div style="height: 15px;"></div>
+		<div style="color: red; font-size: 15px;">
+			<center>
+			<?php
+				session_start();
+				if(isset($_SESSION['msg'])){
+					echo $_SESSION['msg'];
+					unset($_SESSION['msg']);
+				}
+			?>
+			</center>
+		</div>
+	</div>
+</div>
+</body>
 </html>
