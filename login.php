b25e7beea97cac4f85c75965820f3aa16b61e7f8
lucas <lucasrodrigues10@outlook.com>
2017-09-24 18:00:15 -0300

    Coloquei comentarios para melhorar o entendimento

--- login.php
+++ login.php
@@ -1,33 +1,36 @@
 <?php
     session_start();
-	include('conn.php');
+    include('conn.php');
 
-	$usuario = $_POST['username'];
-	$senha = $_POST['password'];
+    //Variáveis que o usuario inseriu
+    $usuario = $_POST['username'];
+    $senha = $_POST['password'];
+    $senha = md5($senha);
 
-	$senha = md5($senha);
-	$query = "SELECT * FROM user WHERE username = '$usuario' AND password = '$senha'";
+    //Query para procurar um usuario e senha iguais
+    $query = "SELECT * FROM user WHERE username = '$usuario' AND password = '$senha'";
+    $result = $conn->query($query);
 
-	$result = $conn->query($query);
-	
-	if($result->num_rows > 0){
-            $row = $result->fetch_assoc();
-            $_SESSION["id"] = $row["userid"];
-            ?>
-            <script>
-				window.alert('Logado com sucesso!');
-				window.location.replace("user/index.php");
-			</script>
-			<?php
-	}else{
-		?>
-            <script>
-				window.alert('Login inválido, tente novamente!');
-				window.location.replace("index.php");
-			</script>
-		<?php
-	}
-
-	$conn->close();
+    if ($result->num_rows > 0) {    //Caso encontre algum resultado no BD
+        //Coloca o resultado na variavel $row
+        $row = $result->fetch_assoc();
+        //Coloca o ID numa session para utilizar nas outras páginas
+        $_SESSION["id"] = $row["userid"];
+        ?>
+        <script>
+            window.alert('Logado com sucesso!');
+            window.location.replace("user/index.php");
+        </script>
+        <?php
+    } else { //Caso não encontre
+        ?>
+        <script>
+            window.alert('Login inválido, tente novamente!');
+            window.location.replace("index.php");
+        </script>
+        <?php
+    }
+    //Fechar conexão com banco de dados
+    $conn->close();
 
 ?>
